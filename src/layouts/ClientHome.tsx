7055575ec392dea590424f69d8789492e20672c9
SirJaKDRipper <137150113+SirJaKDRipper@users.noreply.github.com>
2026-03-06 11:20:29 +0530

    Update ClientHome.tsx

--- src/layouts/ClientHome.tsx
+++ src/layouts/ClientHome.tsx
@@ -412,7 +412,7 @@ export default function ClientHome() {
       </div>
       <div className="my-5 md:mb-6 flex justify-center">
         <Image
-          src="/mobile/group_112.png"
+          src="/mobile/112.png"
           alt="image"
           width={1200}
           height={800}
